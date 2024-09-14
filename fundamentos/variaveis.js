1a88a1319a8aef0718b34c7d950b9f1092377703
Gustavo Nunes <gununes280@gmai.com>
2024-09-13 21:35:03 -0300

    estudando variáveis e constantes

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,22 @@
+/**
+ * Estudo de Constantes e Variáveis
+ * @author Gustavo Nunes Bispo
+*/
+
+console.clear()
+
+let nome
+let idade
+let vip
+
+nome = "Gustavo Nunes"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = "15"
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
