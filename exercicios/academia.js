4047d1d700738aa0e9fe4e883b2c1e93dbfe80a9
GuNunesB <gununes280@gmail.com>
2024-09-19 22:16:34 -0300

    ssss

---
+++ exercicios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha de  um aluno de academia
+ * @author Gustavo Nunes Bispo
+ */
+
+// Variáveis 
+let nome
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+
+// Entrada de dados
+nome = "Gustavo N. Bispo"
+idade = 15
+peso = 80
+altura = 1.70
+vip = true
+
+//saída
+console.log("Ficha do Aluno")
+console.log("______________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`Vip: ${vip}`)
