716567f2cc19576de66d31be98265b6c7cd88cca
GuNunesB <gununes280@gmail.com>
2024-10-15 22:21:51 -0300

    Estruturas Condicionais Compostas

--- exercicios/academia.js
+++ exercicios/academia.js
@@ -34,3 +34,19 @@ console.log(`Altura: ${altura}`)
 console.log(`Vip: ${vip}`)
 console.log(`FCM: ${fcm}`)
 console.log(`IMC: ${imc.toFixed(2)}`)
+
+//Tabela IMC
+
+if(imc < 18.5) {
+    console.log("Abaixo do peso")
+}else if(imc < 25) {
+    console.log("Peso ideal")
+}else if(imc < 30) {
+    console.log("Acima do Peso")
+}else if(imc < 35) {
+    console.log("Obesidade tipo 1")
+}else if(imc < 40) {
+    console.log("Obesidade tipo 2")
+}else{
+    console.log("Obesidade tipo 3")
+}
